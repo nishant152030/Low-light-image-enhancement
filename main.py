abb21077213c18cd59717114159c986137ac1354
nishant152030 <127687512+nishant152030@users.noreply.github.com>
2024-06-18 20:02:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -99,11 +99,23 @@ class ZeroDCE(keras.Model):
         self.spatial_constancy_loss = SpatialConsistencyLoss(reduction="none")
 
     def get_enhanced_image(self, data, output):
-        r = [output[:, :, :, i:i+3] for i in range(0, 24, 3)]
-        x = data
-        for i in range(8):
-            x = x + r[i] * (tf.square(x) - x)
-        return x
+        r1 = output[:, :, :, :3]
+        r2 = output[:, :, :, 3:6]
+        r3 = output[:, :, :, 6:9]
+        r4 = output[:, :, :, 9:12]
+        r5 = output[:, :, :, 12:15]
+        r6 = output[:, :, :, 15:18]
+        r7 = output[:, :, :, 18:21]
+        r8 = output[:, :, :, 21:24]
+        x = data + r1 * (tf.square(data) - data)
+        x = x + r2 * (tf.square(x) - x)
+        x = x + r3 * (tf.square(x) - x)
+        enhanced_image = x + r4 * (tf.square(x) - x)
+        x = enhanced_image + r5 * (tf.square(enhanced_image) - enhanced_image)
+        x = x + r6 * (tf.square(x) - x)
+        x = x + r7 * (tf.square(x) - x)
+        enhanced_image = x + r8 * (tf.square(x) - x)
+        return enhanced_image
 
     def call(self, data):
         dce_net_output = self.dce_model(data)
@@ -133,7 +145,7 @@ class ZeroDCE(keras.Model):
 
     def train_step(self, data):
         with tf.GradientTape() as tape:
-            output = self.call(data)
+            output = self.dce_model(data)
             losses = self.compute_losses(data, output)
         gradients = tape.gradient(
             losses["total_loss"], self.dce_model.trainable_weights
@@ -142,7 +154,7 @@ class ZeroDCE(keras.Model):
         return losses
 
     def test_step(self, data):
-        output = self.call(data)
+        output = self.dce_model(data)
         return self.compute_losses(data, output)
 
     def save_weights(self, filepath, overwrite=True, save_format=None, options=None):
